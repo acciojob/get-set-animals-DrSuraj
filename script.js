95b31301b896cfc7d6292b0dca07342b023c7e38
SURAJ <77872306+DrSuraj@users.noreply.github.com>
2024-02-12 19:49:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,45 @@
+// Animal class
 class Animal {
-  constructor(species) {
-    this._species = species;
-  }
+    constructor(species) {
+        this._species = species;
+    }
 
-  get species() {
-    return this._species;
-  }
+    get species() {
+        return this._species;
+    }
 
-  makeSound() {
-    // Placeholder method, subclasses should override this
-    console.log("Generic animal sound");
-  }
+    makeSound() {
+        console.log(`The ${this.species} makes a sound`);
+    }
 }
 
-class Dog extends Animal {
-  bark() {
-    console.log("Woof");
-  }
-}
-
+// Cat class extending Animal
 class Cat extends Animal {
-  purr() {
-    console.log("Purr");
-  }
+    constructor(species) {
+        super(species);
+    }
+
+    purr() {
+        console.log("purr");
+    }
 }
 
-// Do not change the code below this line
-window.Animal = Animal;
-window.Dog = Dog;
-window.Cat = Cat;
+// Dog class extending Animal
+class Dog extends Animal {
+    constructor(species) {
+        super(species);
+    }
+
+    bark() {
+        console.log("woof");
+    }
+}
+
+// Example usage
+const myCat = new Cat("Siamese");
+myCat.makeSound(); // Output: The Siamese makes a sound
+myCat.purr(); // Output: purr
+
+const myDog = new Dog("Golden Retriever");
+myDog.makeSound(); // Output: The Golden Retriever makes a sound
+myDog.bark(); // Output: woof
